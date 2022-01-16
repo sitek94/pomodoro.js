abb85ce1751ed1c12c89266b858bd3587b2b210a
Maciek <msitkowski94@gmail.com>
2022-01-16 18:09:00 +0100

    format the time, stop when timer hits zero

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// On start click the timer should go down
+const isProd = false
 
 const timerNode = document.querySelector("#timer")
 const startBtn = document.querySelector("#start")
 const stopBtn = document.querySelector("#stop")
 const resetBtn = document.querySelector("#reset")
 
+const INTERVAL = isProd ? 1000 : 0.001
+
 let initialTime = 25 * 60
 let timeLeft = initialTime
-let state = "running"
+let state = "stopped"
 
 startBtn.addEventListener("click", onStart)
 stopBtn.addEventListener("click", onStop)
@@ -16,28 +18,49 @@ resetBtn.addEventListener("click", onReset)
 let timeoutId = null
 
 function onStart() {
+  if (state === "running") {
+    return
+  }
   state = "running"
   tick()
 }
 
-function onStop() {
+function stopTimer() {
   state = "stopped"
   clearTimeout(timeoutId)
+}
+
+function onStop() {
+  if (state === "stopped") {
+    return
+  }
+  stopTimer()
 }
 
 function onReset() {
   state = "stopped"
   clearTimeout(timeoutId)
   timeLeft = initialTime
-  timerNode.textContent = timeLeft
+  updateTimer(initialTime)
 }
 
 function tick() {
+  if (timeLeft === 0) {
+    return stopTimer()
+  }
   if (state === "running") {
     timeoutId = setTimeout(() => {
       timeLeft--
-      timerNode.textContent = timeLeft
+      updateTimer(timeLeft)
       tick()
-    }, 1000)
+    }, INTERVAL)
   }
+}
+
+function updateTimer(timeLeft) {
+  let minutes = Math.floor(timeLeft / 60)
+  let seconds = timeLeft % 60
+
+  let time = [minutes, seconds].map(n => String(n).padStart(2, "0")).join(":")
+  timerNode.textContent = time
 }
