6a90c7730def40fde079daae7e1a7a3dfc4a6e3e
Maciek <msitkowski94@gmail.com>
2022-01-16 17:50:45 +0100

    start, stop and reset the timer when clicking buttons

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-console.log("Hello")
+// On start click the timer should go down
 
-console.log("siema")
+const timerNode = document.querySelector("#timer")
+const startBtn = document.querySelector("#start")
+const stopBtn = document.querySelector("#stop")
+const resetBtn = document.querySelector("#reset")
+
+let initialTime = 25 * 60
+let timeLeft = initialTime
+let state = "running"
+
+startBtn.addEventListener("click", onStart)
+stopBtn.addEventListener("click", onStop)
+resetBtn.addEventListener("click", onReset)
+
+let timeoutId = null
+
+function onStart() {
+  state = "running"
+  tick()
+}
+
+function onStop() {
+  state = "stopped"
+  clearTimeout(timeoutId)
+}
+
+function onReset() {
+  state = "stopped"
+  clearTimeout(timeoutId)
+  timeLeft = initialTime
+  timerNode.textContent = timeLeft
+}
+
+function tick() {
+  if (state === "running") {
+    timeoutId = setTimeout(() => {
+      timeLeft--
+      timerNode.textContent = timeLeft
+      tick()
+    }, 1000)
+  }
+}
