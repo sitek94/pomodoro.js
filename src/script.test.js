c3a2b02efa217b2f476fee43e694c44f444ed6fa
Maciek <msitkowski94@gmail.com>
2022-01-18 21:23:40 +0100

    add test setup helper

--- src/script.test.js
+++ src/script.test.js
@@ -1,9 +1,17 @@
-import { getByText } from "@testing-library/dom"
+import * as tl from "@testing-library/dom"
 import "@testing-library/jest-dom/extend-expect"
 
 describe("index.html", () => {
   it("should have a header", () => {
-    let { container } = global
-    expect(getByText(container, "pomodoro.js 🍅")).toBeInTheDocument()
+    const { getByText } = setup()
+    expect(getByText("pomodoro.js 🍅")).toBeInTheDocument()
   })
 })
+
+function setup() {
+  const { container } = global
+
+  return {
+    getByText: text => tl.getByText(container, text),
+  }
+}
