ecdc3dab2513364592734061d4f5fb29ca699689
Maciek <msitkowski94@gmail.com>
2022-01-25 19:55:40 +0100

    create and test timerView, responsible only for the view (no pomodoro logic here)

---
+++ src/views/timer-view.js
@@ -0,0 +1,29 @@
+export function createTimerView({ timeLeft, onStart, onStop, onReset }) {
+  const view = document.createElement("div")
+  view.id = "timer-view"
+  view.innerHTML = `
+    <h2 id="time-left" class="display">${timeLeft}</h2>
+    <button id="start-btn">start</button>
+    <button id="stop-btn">stop</button>
+    <button id="reset-btn">reset</button>
+  `
+  const timer = view.querySelector("#time-left")
+  function updateTimeLeft(text) {
+    timer.textContent = text
+  }
+
+  const buttons = [
+    ["#start-btn", onStart],
+    ["#stop-btn", onStop],
+    ["#reset-btn", onReset],
+  ]
+  for (const [selector, handler] of buttons) {
+    const button = view.querySelector(selector)
+    button.addEventListener("click", handler)
+  }
+
+  return {
+    view,
+    updateTimeLeft,
+  }
+}
---
+++ src/views/timer-view.test.js
@@ -0,0 +1,67 @@
+import { render, userEvent } from "test-utils"
+import { createTimerView } from "views/timer-view"
+
+const createTimerViewMock = props => {
+  const onStart = jest.fn()
+  const onStop = jest.fn()
+  const onReset = jest.fn()
+
+  const mock = createTimerView({
+    onStart,
+    onStop,
+    onReset,
+    ...props,
+  })
+
+  return {
+    ...mock,
+    onStart,
+    onStop,
+    onReset,
+  }
+}
+
+describe("createTimerView", () => {
+  it(`renders initial view with "time left" set to 300`, () => {
+    const { view } = createTimerViewMock({
+      timeLeft: 300,
+    })
+    const { getByText } = render(view)
+
+    for (let text of ["300", "start", "stop", "reset"]) {
+      expect(getByText(text)).toBeInTheDocument()
+    }
+  })
+
+  it("calls onStart, onStop and onReset", () => {
+    const { view, onStart, onStop, onReset } = createTimerViewMock({
+      timeLeft: 300,
+    })
+    const { getByText } = render(view)
+
+    const startButton = getByText("start")
+    userEvent.click(startButton)
+    expect(onStart).toHaveBeenCalled()
+
+    const stopButton = getByText("stop")
+    userEvent.click(stopButton)
+    expect(onStop).toHaveBeenCalled()
+
+    const resetButton = getByText("reset")
+    userEvent.click(resetButton)
+    expect(onReset).toHaveBeenCalled()
+  })
+
+  it(`updates "timeLeft" using "updateTimeLeft" method`, () => {
+    const { view, updateTimeLeft } = createTimerViewMock({
+      timeLeft: 300,
+    })
+    const { getByText } = render(view)
+
+    updateTimeLeft(100)
+    expect(getByText("100")).toBeInTheDocument()
+
+    updateTimeLeft(200)
+    expect(getByText("200")).toBeInTheDocument()
+  })
+})
